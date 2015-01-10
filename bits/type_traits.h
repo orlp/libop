fcfd32ce1ab8936262b4ab9f50e1fcacf5e75a6c
Orson Peters <orsonpeters@gmail.com>
2015-01-10 14:29:49 +0100

    updated swappable implementation

--- bits/type_traits.h
+++ bits/type_traits.h
@@ -24,25 +24,33 @@ namespace op {
             // If swap ADL finds this then it would call std::swap otherwise (same signature).
             struct tag {};
 
-            template<class T> tag swap(T&, T&) noexcept(
-                std::is_nothrow_move_constructible<T>::value &&
-                std::is_nothrow_move_assignable<T>::value
+            template<class T> tag swap(T&, T&);
+            template<class T, std::size_t N> tag swap(T (&a)[N], T (&b)[N]);
+
+            // Helper functions to test if an unqualified swap is possible, and if it becomes std::swap.
+            template<class, class> std::false_type can_swap(...) noexcept(false);
+            template<class T, class U, class = decltype(swap(std::declval<T&>(), std::declval<U&>()))>
+            std::true_type can_swap(int) noexcept(
+                noexcept(swap(std::declval<T&>(), std::declval<U&>()))
             );
 
-            template<class T, std::size_t N>
-            tag swap(T (&a)[N], T (&b)[N]) noexcept(noexcept(swap(*a, *b)));
-
-            // Helper functions to test if an unqualified swap is possible, and whether it becomes a
-            // std::swap.
             template<class, class> std::false_type uses_std(...);
             template<class T, class U>
             std::is_same<decltype(swap(std::declval<T&>(), std::declval<U&>())), tag> uses_std(int);
 
-            template<class, class> std::false_type can_swap(...) noexcept(false);
-            template<class T, class U,
-                     class = decltype(swap(std::declval<T&>(), std::declval<U&>()))>
-            std::true_type can_swap(int)
-                    noexcept(noexcept(swap(std::declval<T&>(), std::declval<U&>())));
+            // Helper templates to determine if swapping is noexcept. The C++11/14 standards have a
+            // broken noexcept specification for multidimensional arrays, so we use a template solution.
+            template<class T>
+            struct is_std_swap_noexcept : std::integral_constant<bool,
+                std::is_nothrow_move_constructible<T>::value &&
+                std::is_nothrow_move_assignable<T>::value
+            > { };
+
+            template<class T, std::size_t N>
+            struct is_std_swap_noexcept<T[N]> : is_std_swap_noexcept<T> { };
+
+            template<class T, class U>
+            struct is_adl_swap_noexcept : std::integral_constant<bool, noexcept(can_swap<T, U>(0))> { };
         }
     }
 
@@ -62,8 +70,17 @@ namespace op {
 
     template<class T, class U>
     struct is_nothrow_swappable : std::integral_constant<bool, 
-        is_swappable<T, U>::value && noexcept(detail::swap_adl_tests::can_swap<T, U>(0))
+        is_swappable<T, U>::value && (
+            (decltype(detail::swap_adl_tests::uses_std<T, U>(0))::value &&
+                detail::swap_adl_tests::is_std_swap_noexcept<T>::value)
+            ||
+            (!decltype(detail::swap_adl_tests::uses_std<T, U>(0))::value &&
+                detail::swap_adl_tests::is_adl_swap_noexcept<T, U>::value)
+        )
     > {};
 }
 
 #endif
+
+
+
